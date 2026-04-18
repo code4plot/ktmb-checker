71db7476549ebc10fe2d3904e984490660bd316f
code4plot <mail4plot@gmail.com>
2026-04-18 19:29:44 +0800

    removing ipynb checkpoints

--- .ipynb_checkpoints/parser-checkpoint.py
+++
@@ -1,8 +0,0 @@
-def is_time_in_range(hhmm: str, start_hhmm: str, end_hhmm: str) -> bool:
-    value = int(hhmm)
-    start = int(start_hhmm)
-    end = int(end_hhmm)
-
-    if start <= end:
-        return start <= value <= end
-    return value >= start or value <= end
